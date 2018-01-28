e93c0a3c250ebdbe8695f886635c6e33e619d4ac
Jack Roper <roper.jack.h@gmail.com>
2018-01-28 11:11:51 +0000

    Add OnBatCollided event to BatMovement and WinLoseListener.cs

---
+++ Global Game Jam 2018/Assets/WinLoseListener.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class WinLoseListener : MonoBehaviour
+{
+	private void Start ()
+    {
+        BatMovement.Instance.OnBatCollided += OnBatCollided;	
+	}
+
+    private void OnBatCollided(Vector3 point, GameObject other)
+    {
+        if (other.tag == "portal")
+        {
+            // win
+        }
+        else if (other.tag == "fireball")
+        {
+            // do something cool perhaps, then lose?
+        }
+        else
+        {
+            // lose
+        }
+    }
+}
